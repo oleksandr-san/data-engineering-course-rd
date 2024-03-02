c6fbc9ccde379640278fa4ea85911100a0f094b4
Oleksandr Anosov <oleksandr.an@profisealabs.com>
2024-03-02 07:54:58 +0200

    Remove print

--- lesson_02/job1/bll/sales_api.py
+++ lesson_02/job1/bll/sales_api.py
@@ -4,7 +4,6 @@ from job1.dal import local_disk, sales_api
 
 
 def save_sales_to_local_disk(date: str, raw_dir: str) -> None:
-    print("\tI'm in get_sales(...) function!")
     data = sales_api.get_sales(date)
     path = os.path.join(raw_dir, f"sales_{date}.json")
     local_disk.save_to_disk(data, path)
